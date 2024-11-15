1ce6c11fe109c6983c9c18f76c740d681d958e7e
Thang0226 <thang.nd0226@gmail.com>
2024-11-15 09:48:45 +0700

    input validation tested

--- src/controller/ValidateRecord.java
+++ src/controller/ValidateRecord.java
@@ -24,7 +24,7 @@ public class ValidateRecord {
 	}
 
 	private boolean passedDate(String admissionDate) {
-		return Pattern.matches("^(0[1-9])|([12][0-9])|(3[01])/(0[1-9])|(1[0-2])/\\d{4}$", admissionDate);
+		return Pattern.matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$", admissionDate);
 	}
 
 	private boolean passedPeriod(MedicalRecord record) {
